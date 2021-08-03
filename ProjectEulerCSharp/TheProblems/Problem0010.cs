2a52ebbbc87525d642014c9773060a60fb7df2d2
Travis Brooks <traviscodes@gmail.com>
2021-08-02 22:03:18 -0700

    Solved problem 10

---
+++ ProjectEulerCSharp/TheProblems/Problem0010.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace TheProblems
+{
+    [Euler("Summation of primes",
+        @"The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.")]
+    // ReSharper disable once UnusedMember.Global
+    public class Problem0010 : ISolution<long>
+    {
+        /****************************************************************************************************
+         * Summation of primes
+         *
+         * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+         * Find the sum of all the primes below two million.
+         ****************************************************************************************************/
+        public long TheSolution()
+        {
+            var primes = Primes.Calculate(2_000_000).Select(Convert.ToInt64);
+            var sum = primes.Aggregate((acc, prime) => acc + prime);
+
+            return sum;
+        }
+
+        public long ExpectedSolution()
+        {
+            return 142913828922;
+        }
+    }
+}
