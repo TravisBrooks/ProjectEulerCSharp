d8b87d52dc8d9d2bff3f8492290e326fdbf94e13
Travis Brooks <traviscodes@gmail.com>
2021-08-08 01:24:27 -0700

    Came up with analytic solution to problem 2. Pretty nice solution, been a while since i did some math, def more fun than coming up with a quick brute force solution.

--- ProjectEulerCSharp/TheProblems/Problem0002.cs
+++ ProjectEulerCSharp/TheProblems/Problem0002.cs
@@ -21,34 +21,58 @@ find the sum of the even-valued terms.")]
          ****************************************************************************************************/
         public long BruteForceSolution()
         {
-            // This is the more readable version, but runs about 2x slower than the for loop
-            //return Fibonacci.Sequence()
-            //    .TakeWhile(f => f <= 4_000_000)
-            //    .Where(f => f % 2 == 0)
-            //    .Sum();
+            return Fibonacci.Sequence()
+                .Where(n => n < 4_000_000 && n % 2 == 0)
+                .Sum();
+        }
 
-            var total = 0L;
-            var fibs = Fibonacci.Sequence().ToArray();
-            for (var i = 0; i < fibs.Length; i++)
+        public bool HaveImplementedAnalyticSolution => true;
+
+        public long AnalyticSolution()
+        {
+            /****************************************************************************************************
+             * The way that the Fibonacci sequence is presented for this problem doesn't lend itself to this
+             * analytic solution. If I lay out the sequence more traditionally the pattern is more obvious between
+             * the evens vs odds:
+             *      sequence:       0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+             *      even or odd:    E  O  O  E  O  O  E   O   O   E   O   O    E  ...
+             * The pattern emerging is every 3rd fib is even. This makes total sense as the sequence is made of the
+             * sum of its 2 prior elements, and that when you add an Odd and Odd you always get an Even. The
+             * recursive formula for Fib(n) is given as:
+             *  Fib(n) = Fib(n-1) + Fib(n-2)
+             * if Fib(n) is even then we know that Fib(n-1) and Fib(n-2) are odd and Fib(n-3) is even. So the 2
+             * evens prior to and even Fib(n) are Fib(n-3) and Fib(n-6). The trick is to express the original
+             * definition of Fib(n) in terms of Fib(n-3) and Fib(n-6):
+             *      Fib(n) = Fib(n-1) + Fib(n-2)
+             *             = (Fib(n-2) + Fib(n-3)) + (Fib(n-3) + Fib(n-4)) // note Fib(n-3) is listed twice
+             *             = 2Fib(n-3) + Fib(n-2) + Fib(n-4)
+             *             = 2Fib(n-3) + (Fib(n-3) + Fib(n-4)) + (Fib(n-5) + Fib(n-6))
+             *             = 3Fib(n-3) + Fib(n-4) + Fib(n-5) + Fib(n-6)
+             *               ... note that Fib(n-4) + Fib(n-5) is Fib(n-3), so:
+             *             = 4Fib(n-3) + Fib(n-6)
+             * So now we have a definition of the Fibonacci sequence defined only in terms of the even members of
+             * the sequence, so given a seed of the first 2 even members of the sequence we can compute all even
+             * members of the sequence.
+             ****************************************************************************************************/
+
+            long fib_n_minus3 = 2;
+            long fib_n_minus6 = 0;
+            var total = fib_n_minus3 + fib_n_minus6;
+
+            while (fib_n_minus3 < 4_000_000)
             {
-                var fib = fibs[i];
-                if (fib > 4_000_000)
+                var fib_n = 4L * fib_n_minus3 + fib_n_minus6;
+                if (fib_n > 4_000_000)
                 {
                     break;
                 }
 
-                if (fib % 2 == 0)
-                {
-                    total += fib;
-                }
+                fib_n_minus6 = fib_n_minus3;
+                fib_n_minus3 = fib_n;
+                total += fib_n;
             }
 
             return total;
-        }
-
-        public long AnalyticSolution()
-        {
-            throw new System.NotImplementedException();
         }
 
         public long ExpectedSolution()
