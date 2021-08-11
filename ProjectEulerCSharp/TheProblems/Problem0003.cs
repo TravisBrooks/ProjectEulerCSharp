573e7636a7893a4d6bd9db9896e2f66f2d8a8d56
Travis Brooks <traviscodes@gmail.com>
2021-08-11 01:16:41 -0700

    Added brief explanation for problem 3 analytic solution

--- ProjectEulerCSharp/TheProblems/Problem0003.cs
+++ ProjectEulerCSharp/TheProblems/Problem0003.cs
@@ -30,6 +30,17 @@ What is the largest prime factor of the number 600851475143 ?")]
 
         public int AnalyticSolution()
         {
+            /****************************************************************************************************
+             * This solution for finding the factors isn't really "analytical", its still pretty brute force but
+             * its a smarter brute force. Instead of finding all the primes in a range and seeing if they are factors
+             * of the given number we just find all factors of the given number. The alg first checks if the number
+             * is even, if so it whittles n down by factors of 2 until it is no longer divisible by 2. Then it does
+             * the same for 3, then after that we only need to check odd numbers because we've already removed all
+             * factors of 2 from n. We don't actually need to check if the given factor we're looking at is prime
+             * because if it divides the whittled down value of n we know the current factor must be prime. Because
+             * the factor list returned is built up incrementally we also know the last value mut be the largest
+             * prime.
+             ****************************************************************************************************/
             var number = 600851475143L;
             var factors = PrimeFactor64.Factors(number);
             return (int)factors[^1].Factor;
