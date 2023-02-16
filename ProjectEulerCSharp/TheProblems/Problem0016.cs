9044c2a2727fa2a1aa7577cd8ac5ca13141d7983
Travis Brooks <traviscodes@gmail.com>
2023-02-15 22:25:36 -0800

    Came up with a quick solution to #16. Using the builtin BigInteger class to do the work of converting binary to decimal is sort of cheating, but I guess knowing how to easily represent 2^1000 in binary is the real trick to this problem.

---
+++ ProjectEulerCSharp/TheProblems/Problem0016.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Numerics;
+
+namespace TheProblems
+{
+    [Euler(
+title: "Power digit sum",
+description: @"2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?"
+)]
+    // ReSharper disable once UnusedMember.Global
+    public class Problem0016 : ISolution<int>
+    {
+        public bool HaveImplementedAnalyticSolution => false;
+
+        public int BruteForceSolution()
+        {
+            // Because we're dealing with powers of 2 its very easy to represent this number in binary: 1 "1" and 1000 "0s", a total of 1001 bits.
+            // I'll lean on the BigInteger class to go from base10 to base2 so the binary will be little endian.
+            var numberOfBytesNeeded = (int)Math.Ceiling(1001.0 / 8.0);
+            var byteArr = new byte[numberOfBytesNeeded];
+            byteArr[numberOfBytesNeeded - 1] = 1;
+            var bigInt = new BigInteger(byteArr);
+            int sum = 0;
+            foreach (var intChar in bigInt.ToString())
+            {
+                var i = int.Parse(intChar.ToString());
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        public int AnalyticSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExpectedSolution()
+        {
+            return 1366;
+        }
+    }
+}
