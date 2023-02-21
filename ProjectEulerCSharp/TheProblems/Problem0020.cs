f5ab6e1533eb6420ba18a62eb1ca764bd92ae519
Travis Brooks <traviscodes@gmail.com>
2023-02-20 23:50:49 -0800

    Another quick brute force solution to #20

---
+++ ProjectEulerCSharp/TheProblems/Problem0020.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace TheProblems
+{
+    [Euler(
+title: "Factorial digit sum",
+description: @"n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!"
+        )]
+    // ReSharper disable once UnusedMember.Global
+    public class Problem0020 : ISolution<int>
+    {
+        public bool HaveImplementedAnalyticSolution => false;
+
+        public int BruteForceSolution()
+        {
+            return Factorial.Of(100).ToString().Sum(digit => int.Parse(digit.ToString()));
+        }
+
+        public int AnalyticSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExpectedSolution()
+        {
+            return 648;
+        }
+    }
+}
