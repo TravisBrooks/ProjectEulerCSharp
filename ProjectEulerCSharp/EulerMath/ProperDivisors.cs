b1a0d6d7306e71eade4d811b2ad53b937dc11045
Travis Brooks <traviscodes@gmail.com>
2023-02-25 20:36:45 -0800

    Added a brute force solution for #21

---
+++ ProjectEulerCSharp/EulerMath/ProperDivisors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEulerCSharp.EulerMath
+{
+    public static class ProperDivisors
+    {
+        public static IEnumerable<int> Of(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), $"Can only find divisors for a number greater than 0, was passed in {n}.");
+            }
+            yield return 1;
+            for (var i = 2; i<=(int)Math.Ceiling(Math.Sqrt(n)); i++)
+            {
+                if (n % i == 0)
+                {
+                    yield return i;
+                    var maybe = n / i;
+                    if (i != maybe)
+                    {
+                        yield return maybe;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProjectEulerCSharp/EulerMath/UnitTests/ProperDivisorsTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Assert = XunitAssertMessages.AssertM;
+
+namespace ProjectEulerCSharp.EulerMath.UnitTests
+{
+    public class ProperDivisorsTest
+    {
+        [Theory]
+        [MemberData(nameof(ProperDivisorsData))]
+        public void Divisors(ProperDivisorsTestData testData)
+        {
+            var (n, expectedDivisors) = testData;
+            var actualDivisors = ProperDivisors.Of(n);
+            Assert.Equivalent(expectedDivisors, actualDivisors, userMessage: $"unexpected divisors for {n}");
+        }
+
+        public record ProperDivisorsTestData(int N, IList<int> Divisors);
+
+        public static IEnumerable<object[]> ProperDivisorsData()
+        {
+            yield return new object[]
+            {
+                new ProperDivisorsTestData(220, new[]{1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110})
+            };
+            yield return new object[]
+            {
+                new ProperDivisorsTestData(284, new[]{1, 2, 4, 71, 142})
+            };
+            yield return new object[]
+            {
+                new ProperDivisorsTestData(36, new[]{1, 2, 18, 3, 12, 4, 9, 6})
+            };
+        }
+    }
+}
