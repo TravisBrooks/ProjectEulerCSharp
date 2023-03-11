27184a25d4f73ba5528d1e172b0b4aa8d61ce151
Travis Brooks <traviscodes@gmail.com>
2023-03-10 23:03:43 -0800

    Added an analytic solution to #28, revised the brute force to not use tuples and now it runs much faster.

--- ProjectEulerCSharp/EulerProblems/Problem0028.cs
+++ ProjectEulerCSharp/EulerProblems/Problem0028.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ProjectEulerCSharp.EulerProblems
+﻿namespace ProjectEulerCSharp.EulerProblems
 {
     [Euler(
         title: "Problem 28: Number spiral diagonals",
@@ -19,7 +17,7 @@ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
     // ReSharper disable once UnusedMember.Global
     public class Problem0028 : ISolution<int>
     {
-        public bool HaveImplementedAnalyticSolution => false;
+        public bool HaveImplementedAnalyticSolution => true;
 
         public int BruteForceSolution()
         {
@@ -44,27 +42,44 @@ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
             //(13, 17, 21, 25) ~ ( 3 + 10,  5 + 12,  7 + 14,  9 + 16)
             //(31, 37, 43, 49) ~ (13 + 18, 17 + 20, 21 + 22, 25 + 24)
             
-            var diagonals = (1, 1, 1, 1);
+            int a = 1, b = 1, c = 1, d = 1;
             var diagonalDelta = 0;
             var diagonalSum = 1;
-            var dimensions = 1;
-            while (dimensions < 1_001)
+            for (var dimensions = 1; dimensions < 1_001; dimensions+=2)
             {
-                var (a, b, c, d) = diagonals;
                 a += diagonalDelta += 2;
                 b += diagonalDelta += 2;
                 c += diagonalDelta += 2;
                 d += diagonalDelta += 2;
                 diagonalSum += a + b + c + d;
-                diagonals = (a, b, c, d);
-                dimensions += 2;
             }
             return diagonalSum;
         }
 
         public int AnalyticSolution()
         {
-            throw new NotImplementedException();
+            // Try this other solution to see if its any faster. What makes it more analytical is noticing that the upper right
+            // values are squares of the nxn dimension. The 3 other diagonals can be determined by offsets of this n^2 value
+            // 43 44 45 46 47 48 49
+            // 42 21 22 23 24 25 26
+            // 41 20  7  8  9 10 27
+            // 40 19  6  1  2 11 28
+            // 39 18  5  4  3 12 29
+            // 38 17 16 15 14 13 30
+            // 37 36 35 34 33 32 31
+            //
+            //  n^2
+            //  n^2 - 1n + 1
+            //  n^2 - 2n + 2
+            //  n^2 - 3n + 3
+            // added together:
+            // 4n^2 - 6n + 6
+            var diagonalSum = 1;
+            for (var dimensions = 3; dimensions <= 1_001; dimensions+=2)
+            {
+                diagonalSum += 4 * dimensions * dimensions - 6 * dimensions + 6;
+            }
+            return diagonalSum;
         }
 
         public int ExpectedSolution()
