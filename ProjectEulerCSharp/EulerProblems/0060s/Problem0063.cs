5348935bfcadfa684af6658a25f589eb1a5feff6
Travis Brooks <traviscodes@gmail.com>
2025-06-15 00:00:01 -0700

    A quickie brute force solution to #63. Still working on the analytic solution.

---
+++ ProjectEulerCSharp/EulerProblems/0060s/Problem0063.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using ProjectEulerCSharp.EulerMath;
+
+namespace ProjectEulerCSharp.EulerProblems._0060s
+{
+	[Euler(
+			title: "Problem 62: Cubic permutations",
+			description: @"The 5-digit number, 16807, is a fifth power: 27^5 = 16807. Similarly, the 9-digit number, 134217728 = 8^9,, is a ninth power.
+
+How many n-digit positive integers exist which are also an nth power?"
+		)
+	]
+	// ReSharper disable once UnusedType.Global
+	internal class Problem0063 : ISolution<int>
+	{
+		public bool HaveImplementedAnalyticSolution => false;
+
+		public int BruteForceSolution()
+		{
+			var cnt = 0;
+			for(var p = 1; p <= 25; p++)
+			{
+				for(var n = 1; n <= 9; n++)
+				{
+					// have to use BigInteger because n^p can be larger than long.MaxValue
+					var nToP = BigInteger.Pow(new BigInteger(n), p);
+					if (nToP.GetCountOfDigits() == p)
+					{
+						cnt++;
+					}
+				}
+			}
+
+			return cnt;
+		}
+
+		public int AnalyticSolution()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ExpectedSolution()
+		{
+			return 49;
+		}
+	}
+}
