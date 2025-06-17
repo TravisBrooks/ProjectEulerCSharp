3184d1505ca8d73e1f0832c45266b1501aeb8233
Travis Brooks <traviscodes@gmail.com>
2025-06-16 19:35:31 -0700

    Finally came up with an analytical solution for #63. Its been too long since I did proper math proofs so took a lot of research and I'll probably forget some of the details in a week or so. Sigh...

--- ProjectEulerCSharp/EulerProblems/0060s/Problem0063.cs
+++ ProjectEulerCSharp/EulerProblems/0060s/Problem0063.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Numerics;
 using ProjectEulerCSharp.EulerMath;
 
@@ -15,7 +14,7 @@ How many n-digit positive integers exist which are also an nth power?"
 	// ReSharper disable once UnusedType.Global
 	internal class Problem0063 : ISolution<int>
 	{
-		public bool HaveImplementedAnalyticSolution => false;
+		public bool HaveImplementedAnalyticSolution => true;
 
 		public int BruteForceSolution()
 		{
@@ -38,7 +37,40 @@ How many n-digit positive integers exist which are also an nth power?"
 
 		public int AnalyticSolution()
 		{
-			throw new NotImplementedException();
+			// This proof took a bit of effort, I'd forgotten a lot of the rules for equivalent expression wrt logarithms
+			// and inequalities so needed a bit of research.
+
+			// A function for the number of digits in a number n is:
+			// d(n) = floor(log10(n)) + 1
+			// This problem is to find when a number n^p has exactly p digits. Using the previous function:
+			// d(n^p) = floor(log10(n^p)) + 1
+			// log10(n^p) is equivalent to p * log10(n), so:
+			// d(n^p) = floor(p * log10(n)) + 1
+			// We want d(n^p) = p, so:
+			// floor(p * log10(n)) + 1 = p
+			// floor(p * log10(n)) = p - 1
+			// the floor function rounds down, so p * log10(n) must be less than p but greater than or equal to p - 1:
+			// p - 1 <= p * log10(n) < p
+			// Dividing by p gives us:
+			// (p - 1) / p <= log10(n) < 1
+			// or
+			// 1 - 1/p <= log10(n) < 1
+			// Given that log10(10) is 1 and log10(n) < 1 we know n must be less than 10, so focusing on left side of
+			// the inequality:
+			// 1 - 1/p <= log10(n)
+			// subtract 1 from both sides:
+			// -1/p <= log10(n) - 1
+			// multiply both sides by -1 (which flips the inequality):
+			// 1/p >= 1 - log10(n)
+			// inverting both sides gives us:
+			// p >= 1 / (1 - log10(n))
+			var sum = 0;
+			for (var n = 1; n < 10; n++)
+			{
+				sum += (int)Math.Floor(1 / (1 - Math.Log10(n)));
+			}
+
+			return sum;
 		}
 
 		public int ExpectedSolution()
